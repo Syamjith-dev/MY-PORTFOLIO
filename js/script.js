acb00c7dcaa761e85466ccc6f837d88b9098040d
syamjith <syamjithloq@gmail.com>
2026-02-20 21:19:01 +0530

    Updated design

--- js/script.js
+++ js/script.js
@@ -1,15 +1,16 @@
 var tablinks = document.getElementsByClassName("tab-links");
 var tabcontants = document.getElementsByClassName("tab-contents");
 
-function opentab(tabname, el){
-    for(let tablink of tablinks){
+function opentab(tabname){
+    for(tablink of tablinks){
         tablink.classList.remove("active-link");
     }
-    for(let tabcontant of tabcontants){
+    for(tabcontant of tabcontants){
         tabcontant.classList.remove("active-tab");
     }
-    el.classList.add("active-link");
+    event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab");
+
 }
 
 var sidemenu = document.getElementById("sidemenu");
